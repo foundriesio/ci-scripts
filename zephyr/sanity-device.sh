66bb7ce8feb066e9193acd7deb84902ee9f372c2
Andy Doan <andy@foundries.io>
2019-02-19 09:19:01 -0600

    zephyr: Run git_config before doing cherry-pick
    In the event the cherry-pick requires some merging logic, we'll need
    to have git configured.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- zephyr/sanity-device.sh
+++ zephyr/sanity-device.sh
@@ -22,6 +22,7 @@ fi
 
 
 status "Applying board-id patch"
+git_config
 run git fetch https://github.com/zephyrproject-rtos/zephyr.git pull/11851/head:board-id
 run git cherry-pick f4920ec0c869c4292502acb80cfc799a4c52bdc9
 run git cherry-pick 9d8490620ef3b956584b9ef87e084bfba29deee0
