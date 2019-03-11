da91d253e30035c60c16855f51ea04bbeb27a50a
Andy Doan <andy@foundries.io>
2019-03-11 11:03:00 -0500

    lmp: Allow runs to override the host tests
    This allows ota runs to use a private script that can download a hosts
    test file at runtime. This can be used to let multiple types of LMP
    OTA runs to use different (possible private) host testing files.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- lmp/ota.sh
+++ lmp/ota.sh
@@ -3,4 +3,11 @@
 HERE=$(dirname $(readlink -f $0))
 source $HERE/../helpers.sh
 
- PYTHONUNBUFFERED=1 PYTHONPATH=$HERE:$HERE/../ python3 $HERE/ota_test.py
+host_tests="/secrets/download_hosts_tests"
+if [ -f $host_tests ] ; then
+	echo "Overriding hosts_test.py with script from secrets"
+	chmod +x $host_tests
+	$host_tests > HERE/host_tests.py
+fi
+
+PYTHONUNBUFFERED=1 PYTHONPATH=$HERE:$HERE/../ python3 $HERE/ota_test.py
