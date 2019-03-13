93fe01adeda386ceff4c4cfb00396b1f5296269a
Andy Doan <andy@foundries.io>
2019-03-12 22:12:40 -0500

    zephyr: Install latest version of west for samples
    This was missed in 384db5788652f690a42fc336b561ef43d35661ee
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- zephyr/sample.sh
+++ zephyr/sample.sh
@@ -4,6 +4,8 @@ HERE=$(dirname $(readlink -f $0))
 source $HERE/../helpers.sh
 require_params PLATFORM APP
 
+run pip3 install -U west
+
 status "Compiling $APP ..."
 source ./zephyr-env.sh
 mkdir build
