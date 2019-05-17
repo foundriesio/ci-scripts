c0f138150098c2521baace8f429ea725ead1accf
Ricardo Salveti <ricardo@foundries.io>
2019-05-17 15:25:58 -0300

    lmp: drop aktualizr cleanall
    Remove workaround as we update to the latest bitbake, try to reuse cache
    when available.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- lmp/bb-build.sh
+++ lmp/bb-build.sh
@@ -6,8 +6,6 @@ require_params IMAGE
 
 source setup-environment build
 
-bitbake -c cleanall aktualizr
-
 bitbake ${IMAGE}
 
 bitbake -e | grep "^DEPLOY_DIR_IMAGE="| cut -d'=' -f2 | tr -d '"' > image_dir
