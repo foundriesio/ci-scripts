d85ebfe352c0a8f914c742d984898f6a960b6282
Michael Scott <mike@foundries.io>
2019-06-13 09:46:38 -0700

    zmp-samples/fota-test: dont translate PLATFORM into BOARD_NAME
    Let's send PLATFORM directly as the sanity job now works mainly with
    platform names and leaves the abstracted runners to handle their own
    naming internally.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- zmp-samples/fota-test.sh
+++ zmp-samples/fota-test.sh
@@ -1,15 +1,6 @@
 #!/bin/sh -ex
 
-if [ "$PLATFORM" = "nrf52_blenano2" ] ; then
-	BOARD_NAME="RedBearLab-BLE-Nano2"
-fi
-if [ "$PLATFORM" = "reel_board" ] ; then
-	BOARD_NAME="reel board"
-fi
-
-[ -z "$BOARD_NAME" ] && (echo "Invalid PLATFORM=$PLATFORM"; exit 1)
-
 export LOG_DIR=/archive
-python3 -m mp_e2e.sanity ZMP_BUILD=$H_BUILD BOARD_NAME="$BOARD_NAME" \
+python3 -m mp_e2e.sanity ZMP_BUILD=$H_BUILD PLATFORM="$PLATFORM" \
 	CI_PROJECT=$H_PROJECT \
 	CI_RUN=$(basename $H_TRIGGER_URL)
