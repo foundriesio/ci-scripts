01e9e7304a80d6e6b471d50fc6297ebb98afdb72
Mike Sul <mike.sul@foundries.io>
2022-05-27 13:27:00 +0200

    preload-apps: Use a temporal file for auth
    Use a temporal file for authentication material in order to avoid a need
    in access to `/run/containers` folder during the OE builtin preloading.

--- preload-apps.sh
+++ preload-apps.sh
@@ -10,6 +10,12 @@ HERE="$(dirname $(readlink -f $0))"
 
 require_params TARGET_JSON_FILE OCI_STORE_PATH TOKEN_FILE
 
+# Create a temporal file for `skopeo` to store auth material in.
+# The file is removed once preloading is completed.
+# Also, we need to add an empty json `{}` to it, otherwise `skopeo login` fails
+export REGISTRY_AUTH_FILE=$(mktemp)
+echo "{}" > "${REGISTRY_AUTH_FILE}"
+
 PATH="${PATH}:/usr/bin" "${HERE}/preload_apps.py" \
     --target-json-file "${TARGET_JSON_FILE}" \
     --app-shortlist "${APP_SHORTLIST}" \
@@ -17,3 +23,5 @@ PATH="${PATH}:/usr/bin" "${HERE}/preload_apps.py" \
     --token-file "${TOKEN_FILE}" \
     --registry-creds-file "${REGISTRY_SECRETS_FILE}" \
     --log-file "${LOG_FILE}"
+
+trap 'rm -f "${REGISTRY_AUTH_FILE}"' INT TERM HUP EXIT
