c4c178632f63f30d7b10e34e0b02c4efd96a7aa3
Andy Doan <andy@foundries.io>
2022-07-20 22:20:59 -0500

    factory-containers: Update contaienr publishing logic
    We are no longer publishing TUF targets with public LmP builds, so we
    need to change our logic to publish the containers correctly and not
    try to publish compose-apps and target(s)
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ factory-containers/publish-containers.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 Foundries.io,
+# SPDX-License-Identifier: Apache-2.0
+set -o errexit
+set -o pipefail
+
+HERE=$(dirname $(readlink -f $0))
+. $HERE/../helpers.sh
+
+
+status Doing docker-login to hub.foundries.io with secret
+docker login hub.foundries.io --username=doesntmatter --password="$(cat "${SECRETS}/osftok")" | indent
+
+export PYTHONPATH=${HERE}/../
+python3 -c 'from apps.publish_manifest_lists import publish_manifest_lists; publish_manifest_lists("lmp/na")'
