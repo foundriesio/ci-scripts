66cb149c8d0af6abe947bd3df250f6e2769aa057
Mike Sul <mike.sul@foundries.io>
2022-08-01 12:35:26 +0200

    apps: Improve image tagging at the publish step
    - Improve the tagging logic so it takes into account the cases when e
      ref/tag matches an App name and it contains dashes. Also, it should
      fix other odd/edge cases.
    
    - Add a unit test to cover edge cases.
    
    Signed-off-by: Mike <mike.sul@foundries.io>

---
+++ tests/test_manifest_list_publishing.py
@@ -0,0 +1,30 @@
+import unittest
+
+from apps.publish_manifest_lists import compose_tagged_uri
+
+
+class ManifestListPublishing(unittest.TestCase):
+    def test_tagged_uri_composing(self):
+        factory = "_devel-arduino_"
+        app_name = "python-_devel-arduino_"
+        build_num = "459"
+        latest_tag = "_devel-arduino_"
+
+        uris = [
+            (
+                f"hub.foundries.io_{factory}_{app_name}-{build_num}_d09aa17",
+                f"hub.foundries.io/{factory}/{app_name}:{build_num}_d09aa17",
+            ),
+            (
+                # `latest_tag` contains dashes
+                # `app_name` contains `latest_tag`
+                # `latest_tag` matches `factory` with underscores
+                f"hub.foundries.io_{factory}_{app_name}-{latest_tag}",
+                f"hub.foundries.io/{factory}/{app_name}:{latest_tag}",
+            )
+        ]
+
+        for u in uris:
+            res_uri = compose_tagged_uri(factory, u[0], build_num, latest_tag)
+            self.assertEqual(u[1], res_uri)
+
