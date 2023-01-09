4f231a08974f10b6416e2ccdc64b2df2be90a0a0
Jose Quaresma <jose.quaresma@foundries.io>
2023-01-09 11:13:54 +0000

    set_base_lmp_version: fix LMP_VERSION detection
    The version need to be extracted from the upstream lmp-manifest
    otherwise it can come from the user repo that can have invalid
    tags for this propose or even not having any tag.
    
    Also change it and use abbrev=0 to get only the tag name and in
    this way we can use this as well for the sstate cache.
    
    Signed-off-by: Jose Quaresma <jose.quaresma@foundries.io>

--- helpers.sh
+++ helpers.sh
@@ -86,7 +86,8 @@ function set_base_lmp_version {
 	git fetch origin --tags --quiet
 
 	# 3: Find our base LMP version based on the HEAD
-	export LMP_VERSION_CACHE=$(git describe --tags --abbrev=0 HEAD)
+	export LMP_VERSION=$(git describe --tags --abbrev=0 HEAD)
+	export LMP_VERSION_CACHE="$LMP_VERSION"
 	if [[ "${H_PROJECT}" == "lmp" ]] ; then
 		# Public LmP build - we are building for the *next* release
 		LMP_VERSION_CACHE=$(( $LMP_VERSION_CACHE + 1 ))
