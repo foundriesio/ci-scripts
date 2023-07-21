f29d297ebf7dabb45584caee57519343a8c52055
Mike Sul <mike.sul@foundries.io>
2023-07-21 17:55:27 +0200

    lmp: Pass lmp version to the customize target func
    Add a LmP version to the json of the Target is being built by a given CI
    build run.
    
    Signed-off-by: Mike <mike.sul@foundries.io>

--- tests/test_lmp_tagging.py
+++ tests/test_lmp_tagging.py
@@ -69,7 +69,7 @@ def temp_json_file(data):
 def customize_target(tag, targets_file, target_name):
     runurl = "https://api.foundries.io/projects/blah/lmp/builds/459/runs/foo/"
     args = ['./lmp/customize-target.sh', runurl, 'lmp', tag, 'machine_name',
-            'lmp-factory-image', 'amd64', targets_file, target_name,
+            'lmp-factory-image', 'amd64', '91', targets_file, target_name,
             '--manifest-repo=./', '--meta-sub-overrides-repo=./']
     subprocess.check_call(args)
     with open(targets_file) as f:
