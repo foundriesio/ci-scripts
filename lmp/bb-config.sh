acc3e494be52230ae4f8352c38b54a3ae204dafc
Daiane Angolini <daiane.angolini@foundries.io>
2023-09-05 17:57:41 -0300

    lmp: factory-containers: Deprecate ENABLE_PTEST
    Deprecate the variabel ENABLE_PTEST as ptest is disabled by default
    since [1]
    
    [1] https://github.com/foundriesio/meta-lmp/commit/2e51562a43522cdce418bba0784cc6dfa91d1d61
    
    Signed-off-by: Daiane Angolini <daiane.angolini@foundries.io>

--- lmp/bb-config.sh
+++ lmp/bb-config.sh
@@ -15,7 +15,6 @@ LMP_OAUTH_API="${LMP_OAUTH_API-https://app.${dns_base}/oauth}"
 FIO_HUB_URL="${FIO_HUB_URL-${hub_fio}}"
 UBOOT_SIGN_ENABLE="${UBOOT_SIGN_ENABLE-0}"
 DISABLE_GPLV3="${DISABLE_GPLV3-0}"
-ENABLE_PTEST="${ENABLE_PTEST-0}"
 DOCKER_MAX_CONCURRENT_DOWNLOADS="${DOCKER_MAX_CONCURRENT_DOWNLOADS-3}"
 DOCKER_MAX_DOWNLOAD_ATTEMPTS="${DOCKER_MAX_DOWNLOAD_ATTEMPTS-5}"
 MFGTOOL_FLASH_IMAGE="${MFGTOOL_FLASH_IMAGE-lmp-factory-image}"
@@ -33,10 +32,6 @@ BUILD_SDK="${BUILD_SDK-0}"
 
 GARAGE_CUSTOMIZE_TARGET_PARAMS='${MACHINE} ${IMAGE_BASENAME} ${TARGET_ARCH} ${DISTRO_VERSION}'
 TUF_TARGETS_EXPIRE="${TUF_TARGETS_EXPIRE-1Y}"
-
-if [ "$ENABLE_PTEST" = "1" ] ; then
-    OSTREE_BRANCHNAME="${OSTREE_BRANCHNAME}-ptest"
-fi
 
 if [ -f "/secrets/targets.sec" ] ; then
 	status "Generating credentials.zip"
@@ -165,30 +160,6 @@ EOFEOF
 IMAGE_INSTALL:append = " ${EXTRA_IMAGE_INSTALL}"
 EOFEOF
 	fi
-fi
-
-# Ptest-based builds require the same build settings and variables,
-# but the final image needs to be tagged differently, such as
-# <main tag>-ptest, so perform the change at the OTA_LITE_TAG variable
-if [ "${ENABLE_PTEST}" = "1" ]; then
-	IFS=","
-	PTAGS=""
-	for tag in ${OTA_LITE_TAG}; do
-		lmptag=$(echo $tag | cut -d: -f1)
-		PTAGS="${PTAGS} ${lmptag}-ptest"
-		contag=$(echo $tag | cut -s -d: -f1 --complement)
-		if [ -n "${contag}" ]; then
-			PTAGS="${PTAGS}:${contag}"
-		fi
-	done
-	unset IFS
-	OTA_LITE_TAG=$(echo ${PTAGS} | sed -e "s/ /,/g")
-	status "PTEST enabled, OTA_LITE_TAG updated to: ${OTA_LITE_TAG}"
-
-	# Install ptest related packages via extra image features
-	cat << EOFEOF >> conf/local.conf
-EXTRA_IMAGE_FEATURES += " ptest-pkgs"
-EOFEOF
 fi
 
 cat << EOFEOF >> conf/local.conf
