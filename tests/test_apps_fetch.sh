3bc0d2d61e188b784867a3c4f83e7a2cff45d11e
Mike Sul <mike.sul@foundries.io>
2024-02-22 07:00:31 +0100

    apps: Fetch apps specified in targets file
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- tests/test_apps_fetch.sh
+++ tests/test_apps_fetch.sh
@@ -5,25 +5,29 @@ set -euo pipefail
 
 FACTORY=$1
 TOKEN_FILE=$2
-TARGETS=$3
-DST_DIR=$4
-APPS_SHORTLIST=$5
-API_HOST=${6-"api.foundries.io"}
+TARGETS_FILE=$3
+FETCH_DIR=$4
+DST_DIR=$5
+APPS_SHORTLIST=${6-""}
+API_HOST=${7-"api.foundries.io"}
 
 CMD=./apps/fetch.py
 PARAMS="\
   --factory=${FACTORY} \
-  --targets=${TARGETS} \
+  --targets-file=${TARGETS_FILE} \
   --token-file=${TOKEN_FILE} \
-  --dst-dir=/fetched-apps \
-  --apps-shortlist=${APPS_SHORTLIST}
+  --fetch-dir=/fetched-apps \
+  --apps-shortlist=${APPS_SHORTLIST} \
+  --dst-dir=/dst-dir \
 "
 
 docker run -v -it --rm \
   -e PYTHONPATH=. \
   -e H_RUN_URL="https://${API_HOST}" \
   -v "${PWD}":/ci-scripts \
-  -v "${DST_DIR}":/fetched-apps \
+  -v "${FETCH_DIR}":/fetched-apps \
+  -v "${DST_DIR}":/dst-dir \
   -v "${TOKEN_FILE}":"${TOKEN_FILE}" \
+  -v "${TARGETS_FILE}":"${TARGETS_FILE}" \
   -w /ci-scripts \
   foundries/lmp-image-tools ${CMD} ${PARAMS}
